d8da371eaaad534dfe62be2a6d66d34d55672d32
Tarun Jeevan <tjeevan200@gmail.com>
2025-06-01 22:01:30 -0500

    Create data and model
    Created data structure and model class for finance app. Set up SQL Server and connected it to app. Added data annotations to model class for further control over data structure.

---
+++ FinanceApp/Data/FinanceAppContext.cs
@@ -0,0 +1,13 @@
+﻿using FinanceApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceApp.Data
+{
+    public class FinanceAppContext : DbContext
+    {
+        private readonly DbContext _context;
+        public FinanceAppContext(DbContextOptions<FinanceAppContext> options) : base(options) { }
+
+        DbSet<Expense> Expenses { get; set; }
+    }
+}
---
+++ FinanceApp/Models/Expense.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceApp.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+
+        // Set data constraints using data annotations
+        [Required(ErrorMessage = "Expense description required")]
+        public string Description { get; set; } = null!;
+        [Required(ErrorMessage = "Missing amount in expense")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount must be positive")]
+        public double Amount { get; set; }
+        [Required(ErrorMessage = "Expense category must be set")]
+        public string Category { get; set; } = null!; // Replace with Enum?
+
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
